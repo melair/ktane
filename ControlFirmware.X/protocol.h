5857d24b2ac99d732731405b906fd34f93f63fc7
Melair <melair@caldonia.net>
2021-06-28 09:17:52 +0100

    Refactor protocol to split different prefixes into seperate files.

--- ControlFirmware.X/protocol.h
+++ ControlFirmware.X/protocol.h
@@ -3,12 +3,10 @@
 
 void protocol_receive(uint8_t prefix, uint8_t id, uint8_t size, uint8_t *payload);
 
-void protocol_module_announcement_send(void);
-void protocol_module_error_send(uint16_t code);
-
-#define PREFIX_MODULE 0b000
-#define PREFIX_GAME   0b010
-#define PREFIX_DEBUG  0b111
+#define PREFIX_MODULE       0b000
+#define PREFIX_GAME         0b010
+#define PREFIX_FIRMWARE     0b110
+#define PREFIX_DEBUG        0b111
 
 #endif	/* PROTOCOL_H */
 
