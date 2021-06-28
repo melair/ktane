4e0985ab15c3a5347b7b8623b1e8e2f661f59b46
Melair <melair@caldonia.net>
2021-06-28 21:11:52 +0100

    Add support for module identify message, adapt status identifier to display.

--- ControlFirmware.X/protocol_module.h
+++ ControlFirmware.X/protocol_module.h
@@ -5,6 +5,7 @@ void protocol_module_receive(uint8_t id, uint8_t size, uint8_t *payload);
 void protocol_module_announcement_send(void);
 void protocol_module_error_send(uint16_t code);
 void protocol_module_reset_send(void);
+void protocol_module_identify_send(uint8_t id);
 
 #endif	/* PROTOCOL_MODULE_H */
 
