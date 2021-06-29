fe95bacc520c3967501ba8ee3d1d901f6ee89a99
Melair <melair@caldonia.net>
2021-06-29 19:55:50 +0100

    Text formatting.

--- ControlFirmware.X/buzzer.c
+++ ControlFirmware.X/buzzer.c
@@ -1,6 +1,6 @@
 /**
  * Provides support for driving a piezo sounder via PWM.
- * 
+ *
  * USES: PWM2S1
  */
 #include <xc.h>
@@ -13,9 +13,9 @@ uint24_t buzzer_off_tick = 0;
 #define TOTAL_FREQUENCIES 3
 
 const uint16_t frequencies[TOTAL_FREQUENCIES][2] = {
-    {BUZZER_FREQ_A6_SHARP, 0x010c}, // A#6
-    {BUZZER_FREQ_C7_SHARP, 0x00e1}, // C#7
-    {BUZZER_FREQ_B8, 0x003f}, // B8
+    {BUZZER_FREQ_A6_SHARP, 0x010c},
+    {BUZZER_FREQ_C7_SHARP, 0x00e1},
+    {BUZZER_FREQ_B8,       0x003f},
 };
 
 /**
@@ -24,26 +24,26 @@ const uint16_t frequencies[TOTAL_FREQUENCIES][2] = {
 void buzzer_initialise(void) {
     /* Set RB4 to output. */
     TRISBbits.TRISB4 = 0;
-      
-    /* Set RB4 to use PWM2SP1. */      
+
+    /* Set RB4 to use PWM2SP1. */
     RB4PPS = 0x1a;
- 
+
     /* Disable external reset. */
     PWM2ERS = 0;
-    
+
     /* Set PWM clock to FSOC, 64MHz. */
     PWM2CLKbits.CLK = 0b00010;
-    
+
     /* Disable auto load. */
     PWM2LDS = 1;
-    
+
     /* Prescale to 1:128. */
-    PWM2CPRE = 127; 
+    PWM2CPRE = 127;
 }
 
 /**
  * Turn buzzer on.
- * 
+ *
  * @param volume volume of buzzer, 0 to 255
  * @param frequency frequency of buzzer in Hz
  */
@@ -61,18 +61,18 @@ void buzzer_on(uint8_t volume, uint16_t frequency) {
     if (PWM2PR == 0) {
         return;
     }
-   
+
     PWM2S1P1 = PWM2PR-1;
 
     PWM2CONbits.EN = 1;
-    
+
     buzzer_off_tick = 0;
 }
 
 /**
  * Turn buzzer on, timed.
- * 
- * @param duration amount of time 
+ *
+ * @param duration amount of time
  */
 void buzzer_on_timed(uint8_t volume, uint16_t frequency, uint16_t duration) {
     buzzer_on(volume, frequency);
@@ -84,7 +84,7 @@ void buzzer_on_timed(uint8_t volume, uint16_t frequency, uint16_t duration) {
  */
 void buzzer_off(void) {
     PWM2CONbits.EN = 0;
-    
+
     buzzer_off_tick = 0;
 }
 
@@ -94,7 +94,7 @@ void buzzer_off(void) {
 void buzzer_service(void) {
     if (buzzer_off_tick > 0) {
         if (tick_fetch() > buzzer_off_tick) {
-            buzzer_off();            
+            buzzer_off();
         }
     }
 }
