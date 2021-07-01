17bd1db26c0d05a254d7b5500adbacdad5523704
Melair <melair@caldonia.net>
2021-07-01 15:23:42 +0100

    First pass at seven segment display driver for timer board.

---
+++ ControlFirmware.X/peripherals/ports.h
@@ -0,0 +1,45 @@
+#ifndef PORTS_H
+#define	PORTS_H
+
+#define KPORTA PORTA
+#define KPORTB PORTF
+#define KPORTC PORTD
+
+#define KPORTAbits PORTAbits
+#define KPORTBbits PORTFbits
+#define KPORTCbits PORTDbits
+
+#define KTRISA TRISA
+#define KTRISB TRISF
+#define KTRISC TRISD
+
+#define KTRISAbits TRISAbits
+#define KTRISBbits TRISFbits
+#define KTRISCbits TRISDbits
+
+#define KLATA LATA
+#define KLATB LATF
+#define KLATC LATD
+
+#define KLATAbits LATAbits
+#define KLATBbits LATFbits
+#define KLATCbits LATDbits
+
+#define KODCONA ODCONA
+#define KODCONB ODCONF
+#define KODCONC ODCOND
+
+#define KODCONAbits ODCONAbits
+#define KODCONBbits ODCONFbits
+#define KODCONCbits ODCONDbits
+
+#define KWPUA WPUA
+#define KWPUB WPUF
+#define KWPUC WPUD
+
+#define KWPUAbits WPUAbits
+#define KWPUBbits WPUFbits
+#define KWPUCbits WPUDbits
+
+#endif	/* PORTS_H */
+
