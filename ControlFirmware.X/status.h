7470411f876baffd731a91a3668fbdd9f935259a
Melair <melair@caldonia.net>
2021-07-03 19:12:58 +0100

    Add new statuses for ready/unready modules.

--- ControlFirmware.X/status.h
+++ ControlFirmware.X/status.h
@@ -6,11 +6,12 @@ void status_error(uint8_t);
 void status_identify(bool on);
 void status_service(void);
 
-#define STATUS_READY        0
-#define STATUS_UNSOLVED     1
-#define STATUS_SOLVED       2
-#define STATUS_IDENTIFY     3
-#define STATUS_UNUSED       4
+#define STATUS_IDLE         0
+#define STATUS_SETUP        1
+#define STATUS_READY        2
+#define STATUS_UNSOLVED     3
+#define STATUS_SOLVED       4
+#define STATUS_UNUSED       5
 
 #define ERROR_NONE            0
 #define ERROR_LOCAL           1
