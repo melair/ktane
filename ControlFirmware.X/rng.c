a8aaba996744007a3903c95755881b19b987839e
Melair <melair@caldonia.net>
2021-07-04 21:21:23 +0100

    Add random generator based on xorshift32.

---
+++ ControlFirmware.X/rng.c
@@ -0,0 +1,29 @@
+#include <xc.h>
+#include <stdint.h>
+#include "rng.h"
+
+/**
+ * Generate a random number based upon the seed provided.
+ * 
+ * WARNING: This is Xorshift32 - it is not cryptographically secure - don't use
+ * it for anything important. https://en.wikipedia.org/wiki/Xorshift
+ * 
+ * @param seed pointer to seed, will be mutated by function
+ * @param mask a mask value to XOR the seed on return, obsfucates the seed to consumers of data
+ * @return 32 bits random of data
+ */
+uint32_t rng_generate(uint32_t *seed, uint32_t mask) {
+    /* Copy seed locally. */
+    uint32_t local_seed = *seed;
+    
+    /* Xorshift32 RNG implemenation. */
+    local_seed ^= local_seed << 13;
+	local_seed ^= local_seed >> 17;
+	local_seed ^= local_seed << 5;  
+    
+    /* Mutate original seed. */
+    *seed = local_seed;
+    
+    /* Return the new seed xor'd with the application mask. */
+    return local_seed ^ mask;
+}
---
+++ ControlFirmware.X/rng.h
@@ -0,0 +1,7 @@
+#ifndef RNG_H
+#define	RNG_H
+
+uint32_t rng_generate(uint32_t *seed, uint32_t mask);
+
+#endif	/* RNG_H */
+
