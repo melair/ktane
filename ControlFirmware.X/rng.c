c6ccd26dc40e37bf8622260e009bb7e50e231fb9
Melair <melair@caldonia.net>
2021-07-04 21:23:44 +0100

    Minor annotation on rng doc.

--- ControlFirmware.X/rng.c
+++ ControlFirmware.X/rng.c
@@ -9,7 +9,9 @@
  * it for anything important. https://en.wikipedia.org/wiki/Xorshift
  * 
  * @param seed pointer to seed, will be mutated by function
- * @param mask a mask value to XOR the seed on return, obsfucates the seed to consumers of data
+ * @param mask a mask value to XOR the seed on return, obsfucates the seed to 
+ *        consumers of data - this allows multiple systems to generate different
+ *        data from the same seed
  * @return 32 bits random of data
  */
 uint32_t rng_generate(uint32_t *seed, uint32_t mask) {
