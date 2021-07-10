6e9865897e0e0a4513b399865b32374a4d0eadbb
Melair <melair@caldonia.net>
2021-07-10 23:12:25 +0100

    Add abstraction over MCU pins.

---
+++ ControlFirmware.X/peripherals/ports.c
@@ -0,0 +1,123 @@
+#include <xc.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include "ports.h"
+
+#define kpin_is_porta(PIN)      ((PIN & 0b00111000) == 0b00000000)
+#define kpin_is_portb(PIN)      ((PIN & 0b00111000) == 0b00001000)
+#define kpin_is_portc(PIN)      ((PIN & ) == 0b00010000)
+
+#define KPIN_PORT_MASK          0b00111000
+#define KPIN_PIN_MASK(PIN)      (1 << (PIN & 0b00000111))
+
+/**
+ * Provide configuration support for pins on KTANE ports.
+ * 
+ * @param port port to change
+ * @param mode INPUT or OUTPUT mode
+ * @param pullup true if weak pull up should be enabled
+ */
+void kpin_mode(pin_t pin, uint8_t mode, bool pullup) {
+    uint8_t mask = KPIN_PIN_MASK(pin);
+    
+    switch(pin & KPIN_PORT_MASK) {
+        case 0b00000000:
+            if (mode == PIN_OUTPUT) {
+                KTRISA &= ~mask;
+            } else {
+                KTRISA |= mask;
+            }
+            
+            if (pullup) {
+                KWPUA |= mask;
+            } else {
+                KWPUA &= ~mask;
+            }
+            break;
+        case 0b00001000:
+            if (mode == PIN_OUTPUT) {
+                KTRISB &= ~mask;
+            } else {
+                KTRISB |= mask;
+            }
+            
+            if (pullup) {
+                KWPUB |= mask;
+            } else {
+                KWPUB &= ~mask;
+            }         
+            break;            
+        case 0b00010000:
+            if (mode == PIN_OUTPUT) {
+                KTRISC &= ~mask;
+            } else {
+                KTRISC |= mask;
+            }
+            
+            if (pullup) {
+                KWPUC |= mask;
+            } else {
+                KWPUC &= ~mask;
+            }
+            break;            
+    }
+}
+
+/**
+ * Read from KTANE pin.
+ * 
+ * @param pin to read from
+ * @return  true if read value is high
+ */
+bool kpin_read(pin_t pin) {
+    uint8_t mask = KPIN_PIN_MASK(pin);
+    uint8_t val;
+    
+    switch(pin & KPIN_PORT_MASK) {
+        case 0b00000000:
+            val = KPORTA;
+            break;
+        case 0b00001000:
+            val = KPORTB;         
+            break;            
+        case 0b00010000:
+            val = KPORTC;
+            break;            
+    }
+    
+    return ((val & mask) == mask);
+}
+
+/**
+ * Write to KTANE pin.
+ * 
+ * @param pin to write to
+ * @param value high or low value to write
+ */
+void kpin_write(pin_t pin, bool value) {
+    uint8_t mask = KPIN_PIN_MASK(pin);
+    
+    switch(pin & KPIN_PORT_MASK) {
+        case 0b00000000:
+            if (value) {
+                KLATA |= mask;
+            } else {
+                KLATA &= ~mask;
+            }
+            break;
+        case 0b00001000:
+            if (value) {
+                KLATB |= mask;
+            } else {
+                KLATB &= ~mask;
+            }
+            break;           
+        case 0b00010000:
+            if (value) {
+                KLATC |= mask;
+            } else {
+                KLATC &= ~mask;
+            }
+            break;         
+    }
+}
--- ControlFirmware.X/peripherals/ports.h
+++ ControlFirmware.X/peripherals/ports.h
@@ -41,5 +41,43 @@
 #define KWPUBbits WPUFbits
 #define KWPUCbits WPUDbits
 
+typedef uint8_t pin_t;
+
+#define KPIN_A0 ((pin_t) 0b00000000)
+#define KPIN_A1 ((pin_t) 0b00000001)
+#define KPIN_A2 ((pin_t) 0b00000010)
+#define KPIN_A3 ((pin_t) 0b00000011)
+#define KPIN_A4 ((pin_t) 0b00000100)
+#define KPIN_A5 ((pin_t) 0b00000101)
+#define KPIN_A6 ((pin_t) 0b00000110)
+#define KPIN_A7 ((pin_t) 0b00000111)
+
+#define KPIN_B0 ((pin_t) 0b00001000)
+#define KPIN_B1 ((pin_t) 0b00001001)
+#define KPIN_B2 ((pin_t) 0b00001010)
+#define KPIN_B3 ((pin_t) 0b00001011)
+#define KPIN_B4 ((pin_t) 0b00001100)
+#define KPIN_B5 ((pin_t) 0b00001101)
+#define KPIN_B6 ((pin_t) 0b00001110)
+#define KPIN_B7 ((pin_t) 0b00001111)
+
+#define KPIN_C0 ((pin_t) 0b00010000)
+#define KPIN_C1 ((pin_t) 0b00010001)
+#define KPIN_C2 ((pin_t) 0b00010010)
+#define KPIN_C3 ((pin_t) 0b00010011)
+#define KPIN_C4 ((pin_t) 0b00010100)
+#define KPIN_C5 ((pin_t) 0b00010101)
+#define KPIN_C6 ((pin_t) 0b00010110)
+#define KPIN_C7 ((pin_t) 0b00010111)
+
+#define KPIN_NONE 0b11111111
+
+#define PIN_OUTPUT 0
+#define PIN_INPUT  1
+
+void kpin_mode(pin_t port, uint8_t mode, bool pullup);
+bool kpin_read(pin_t port);
+void kpin_write(pin_t port, bool value);
+
 #endif	/* PORTS_H */
 
