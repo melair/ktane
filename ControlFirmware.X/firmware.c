e4658caede319bf8076ad07f9e8c78b6ae347a6a
Melair <melair@caldonia.net>
2023-03-11 11:45:27 +0000

    Bump firmware version.

--- ControlFirmware.X/firmware.c
+++ ControlFirmware.X/firmware.c
@@ -21,7 +21,7 @@ asm("GLOBAL _firmware_flash");
 #ifdef __DEBUG
 const uint16_t firmware_version = INVALID_FIRMWARE_VERSION;
 #else
-const uint16_t firmware_version = 0x004b;
+const uint16_t firmware_version = 0x004c;
 #endif
 
 /* CRC32 checksum of firmware, first 63kb of program memory. */
