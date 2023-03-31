8d6ccf3641897eefe476fecc8cbce9f4c9cb5dcb
Melair <melair@caldonia.net>
2023-03-31 22:11:10 +0100

    Optomise version location selection, saves 20 bytes.

--- Firmware/common/versions.c
+++ Firmware/common/versions.c
@@ -4,25 +4,15 @@
 #include "segments.h"
 #include "versions.h"
 
+/* Cached versions. */
 uint16_t versions[3];
+
+/* Locations in PFM memory for versions. */
+const uint32_t version_locations[3] = { BOOTLOADER_OFFSET + BOOTLOADER_SIZE - 2, APPLICATION_OFFSET + APPLICATION_SIZE - 2, FLASHER_OFFSET + FLASHER_SIZE - 2 };
 
 void versions_initialise(void) {
     for (uint8_t i = 0; i < 3; i++) {
-        uint32_t addr;
-        
-        switch(i) {
-            case BOOTLOADER_VERSION:
-                addr = BOOTLOADER_OFFSET + BOOTLOADER_SIZE - 2;
-                break;
-            case APPLICATION_VERSION:
-                addr = APPLICATION_OFFSET + APPLICATION_SIZE - 2;
-                break;
-            case FLASHER_VERSION:
-                addr = FLASHER_OFFSET + FLASHER_SIZE - 2;
-                break;
-        }
-        
-        versions[i] = nvm_pfm_read(addr);
+        versions[i] = nvm_pfm_read(version_locations[i]);
     }
 }
 
