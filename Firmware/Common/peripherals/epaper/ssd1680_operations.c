1a58b636112e36e108dab80303dee92699caee84
Melair <melair@caldonia.net>
2026-03-24 20:10:04 +0000

    Add operations files.

---
+++ Firmware/Common/peripherals/epaper/ssd1680_operations.c
@@ -0,0 +1,16 @@
+#include <xc.h>
+#include "../../utils/fsm.h"
+#include "../../hal/spi.h"
+#include "peripherals/epaper/ssd1680.h"
+#include "ssd1680_operations.h"
+
+void ssd1680_operation_fill_enter(fsm_t *fsm) {
+  epaper_t *epaper = (epaper_t *)fsm->ctx;
+
+
+}
+
+const fs_t ssd1680_operation_fill = {
+    .name = "QUEUE",
+    .next_states = {&ssd1680_queue_return, NULL},
+    .enter = ssd1680_operation_fill_enter};
---
+++ Firmware/Common/peripherals/epaper/ssd1680_operations.h
@@ -0,0 +1,6 @@
+#ifndef SSD1680_OPERATIONS_H
+#define SSD1680_OPERATIONS_H
+
+extern const fs_t ssd1680_operation_fill;
+
+#endif
